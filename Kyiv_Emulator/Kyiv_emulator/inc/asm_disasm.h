cb05fedcca99ef394076e4621f500c3dfb5790d9
MaksHobela <149590733+MaksHobela@users.noreply.github.com>
2025-10-08 20:41:29 +0300

    Added emulator

---
+++ Kyiv_Emulator/Kyiv_emulator/inc/asm_disasm.h
@@ -0,0 +1,10 @@
+#ifndef KYIVEMU_ASM_DISASM_H
+#define KYIVEMU_ASM_DISASM_H
+
+int disassembly(const uint64_t & command_oct, Kyiv_memory_t & kmem, const addr3_t &addr3);
+int check_modification(std::string addr);
+int disassembly_text(const std::string file_from, const std::string file_to);
+std::string mod_comment(std::string addr_1, std::string addr_2, std::string addr_3);
+int disassembler_second_pass(std::map<std::string, std::string> program, std::map<std::string, std::string> jumps, const std::string& file_to);
+int write_file(const char* filename, std::string &res);
+#endif //KYIVEMU_ASM_DISASM_H
